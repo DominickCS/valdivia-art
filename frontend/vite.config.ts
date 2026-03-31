82edbfb84860643a2451ed8fef019f6288a0baf0
Dominick <dominick.smith938@gmail.com>
2026-03-31 10:37:53 -0500

    update vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   server: {
     host: '0.0.0.0',
+    allowedHosts: ["art.dominickcs.com"],
     port: 5173,
   },
   plugins: [
