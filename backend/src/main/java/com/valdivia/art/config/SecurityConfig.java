2b53025108bb7034c4902100062c4938aacdc6d7
Dominick Smith <mail@dominickcs.com>
2026-05-09 10:06:29 -0500

    Added PATCH method to CORS Configuration context;

--- backend/src/main/java/com/valdivia/art/config/SecurityConfig.java
+++ backend/src/main/java/com/valdivia/art/config/SecurityConfig.java
@@ -80,7 +80,7 @@ public class SecurityConfig {
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration config = new CorsConfiguration();
     config.setAllowedOrigins(List.of("http://art.dominickcs.com:5173", "http://localhost:5173"));
-    config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+    config.setAllowedMethods(List.of("GET", "PATCH", "POST", "PUT", "DELETE", "OPTIONS"));
     config.setAllowedHeaders(List.of("*"));
     config.setAllowCredentials(true);
 
