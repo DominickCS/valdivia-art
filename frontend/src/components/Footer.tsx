de18e0205ea1fa434bb6684a0f03c0fc88b8d426
Dominick Smith <mail@dominickcs.com>
2026-06-06 13:57:13 -0500

    Footer and Carousel UI fixes

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -8,29 +8,24 @@ export default function Footer() {
   return (
     <footer className="w-full border-t-2 border-black/10 shadow-inner px-8 py-6 mt-auto">
       <div className="flex flex-col lg:flex-row items-center justify-between gap-4 text-sm text-black/50">
-        <p className="text-sm">© {year} Daniel Valdivia · Valdivia.co</p>
-        <div className="flex justify-between [&>a]:px-4">
-          <Link to={"https://www.instagram.com/danielvaldivia"} target="_blank" className="transition-all hover:text-black hover:tracking-widest duration-300" >
-            <div className="flex justify-between items-center content-center text-center">
-              <img className="mr-2" src={InstagramSVG} height={18} width={18} />
+        <p className="text-sm text-center">© {year} Daniel Valdivia · Valdivia.co</p>
+        <div className="flex items-center justify-center gap-6">
+          <Link to={"https://www.instagram.com/danielvaldivia"} target="_blank" className="transition-all hover:text-black hover:tracking-widest duration-300">
+            <div className="flex items-center gap-2">
+              <img src={InstagramSVG} height={18} width={18} />
               <p className="md:text-lg font-bold">@danielvaldivia</p>
             </div>
           </Link>
-          <Link to={"https://www.youtube.com/@danielvaldiviapaints"} target="_blank" className="transition-all hover:text-black hover:tracking-widest duration-300" >
-            <div className="flex justify-between items-center">
-              <img className="flex items-center content-center mr-2" src={YouTubeSVG} height={20} width={20} />
+          <Link to={"https://www.youtube.com/@danielvaldiviapaints"} target="_blank" className="transition-all hover:text-black hover:tracking-widest duration-300">
+            <div className="flex items-center gap-2">
+              <img src={YouTubeSVG} height={20} width={20} />
               <p className="md:text-lg font-bold">@danielvaldiviapaints</p>
             </div>
           </Link>
         </div>
-        <p className="text-sm">
+        <p className="text-sm text-center">
           Developed by{" "}
-          <a
-            href="https://dominickcs.com"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="transition-all hover:text-black hover:tracking-widest duration-300"
-          >
+          <a href="https://dominickcs.com" target="_blank" rel="noopener noreferrer" className="transition-all hover:text-black hover:tracking-widest duration-300">
             DominickCS
           </a>
         </p>
--- frontend/src/components/ui/carousel.tsx
+++ frontend/src/components/ui/carousel.tsx
@@ -215,7 +215,7 @@ function CarouselNext({
       className={cn(
         "absolute touch-manipulation rounded-full bg-black/20",
         orientation === "horizontal"
-          ? "top-1/2 -right-8.5 -translate-y-1/2"
+          ? "top-1/2 -right-9 -translate-y-1/2"
           : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
         className
       )}
